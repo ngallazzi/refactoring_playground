e8faa96464dc6bb68e4dfd11af85f88ead872c1c
Nicola Gallazzi <nicola.gallazzi.dev@gmail.com>
2021-12-30 18:47:16 +0100

    feat(ExtractFunction): Replace the extracted code with the new function

--- app/src/main/java/com/ngallazzi/myapplication/MainActivity.kt
+++ app/src/main/java/com/ngallazzi/myapplication/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : AppCompatActivity() {
     private fun printInvoiceSheet(invoice: Invoice) {
         printInvoiceHeader(invoice)
         // print customer details
-
+        printCustomerDetails(invoice.customer)
     }
 
     private fun printInvoiceHeader(invoice: Invoice) {
