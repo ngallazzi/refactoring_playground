e136d7bec24914a6147296be36bd9f25ad8de738
Nicola Gallazzi <nicola.gallazzi.dev@gmail.com>
2021-12-30 17:22:24 +0100

    feat: First commit

---
+++ app/src/main/java/com/ngallazzi/myapplication/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ngallazzi.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
