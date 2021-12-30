4bf4ec70f946941018a0793d6727ee3b73242321
Nicola Gallazzi <nicola.gallazzi.dev@gmail.com>
2021-12-30 18:32:58 +0100

    feat(ExtractFunction): Starting code

--- app/src/main/java/com/ngallazzi/myapplication/MainActivity.kt
+++ app/src/main/java/com/ngallazzi/myapplication/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.ngallazzi.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.jakewharton.threetenabp.AndroidThreeTen
+import com.ngallazzi.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        AndroidThreeTen.init(this)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        printInvoiceSheet(Utils.getRandomInvoice(this))
+    }
+
+    private fun printInvoiceSheet(invoice: Invoice) {
+        printInvoiceHeader(invoice)
+        // print customer details
+        var customerDetails = ""
+        customerDetails += "CUSTOMER\n"
+        customerDetails += "First Name: ${invoice.customer.firstName}\nLast Name: ${invoice.customer.lastName}\n"
+        customerDetails += "Address: ${invoice.customer.address}\n"
+        binding.tvInvoicePreview.append(customerDetails)
+    }
+
+    private fun printInvoiceHeader(invoice: Invoice) {
+        val invoiceHeader = "INVOICE N° ${invoice.number}, ${invoice.date}\n\n"
+        binding.tvInvoicePreview.text = invoiceHeader
     }
 }
