f12bfb4cdd1a0220150d1865aea4b5f4f2736530
Nicola Gallazzi <n.gallazzi@orange-its.ch>
2022-01-11 18:12:07 +0100

    feat(Inline function): Starting code

--- app/src/main/java/com/ngallazzi/myapplication/MainActivity.kt
+++ app/src/main/java/com/ngallazzi/myapplication/MainActivity.kt
@@ -19,11 +19,21 @@ class MainActivity : AppCompatActivity() {
     private fun printInvoiceSheet(invoice: Invoice) {
         printInvoiceHeader(invoice)
         // print customer details
-        var customerDetails = ""
-        customerDetails += "CUSTOMER\n"
-        customerDetails += "First Name: ${invoice.customer.firstName}\nLast Name: ${invoice.customer.lastName}\n"
-        customerDetails += "Address: ${invoice.customer.address}\n"
-        binding.tvInvoicePreview.append(customerDetails)
+        val fullName = getFullName(invoice)
+        binding.tvInvoicePreview.append(fullName)
+        val address = getAddress(invoice)
+        binding.tvInvoicePreview.append(address)
+    }
+
+    private fun getFullName(invoice: Invoice): String {
+        val fullName =
+            "First Name: ${invoice.customer.firstName}\nLast Name: ${invoice.customer.lastName}\n"
+        return fullName
+    }
+
+    private fun getAddress(invoice: Invoice) : String {
+        val address = "Address: ${invoice.customer.address}\n"
+        return address
     }
 
     private fun printInvoiceHeader(invoice: Invoice) {
